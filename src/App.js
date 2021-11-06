f5aca950f189e9dd220ae506334ec3b0787322ef
Lior Kastenbaum <liorkasti@gmail.com>
2021-11-06 15:15:30 +0200

    Fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo } from "react";
+import "./styles.css";
+import loader from "./assets/loader.gif";
 
-function App() {
+const api = `https://restcountries.com/v3.1/all`;
+
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // useEffect for fetching data on mount
+  useEffect(() => {
+    fetch(api)
+      .then((response) => response.json())
+      .then((data) => setData(data), console.log("data: ", data));
+    setLoading(true);
+  }, []);
+
+  const handleSearchChange = (e) => {
+    const newValue = e.target.value;
+    setSearch(newValue);
+    console.log("search : ", search);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Italia</h1>
+      <input
+        type="text"
+        placeholder="Search a country by name"
+        value={search}
+        onChange={handleSearchChange}
+      />
+      {loading ? (
+        data.map((nation) => (
+          <div key={nation.name.common}>{nation.name.common}</div>
+        ))
+      ) : (
+        <div>
+          Loading...
+          <img src={loader} className="img-fluid" alt="Responsive image" />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
