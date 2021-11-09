c6d611714cfd508920d0ffdfd7e235b14266dc18
Lior Kastenbaum <liorkasti@gmail.com>
2021-11-09 23:34:45 +0200

    Add useMemo for fetching data on mount

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect, useMemo } from "react";
 import { from, BehaviorSubject, timer, of, zip } from "rxjs";
 import {
   filter, debounceTime, mergeMap, distinctUntilChanged,
-  tap, map, catchError, startWith, switchMap
+  tap, map, catchError, startWith, switchMap, takeUntil
 } from "rxjs/operators";
 import { fromFetch } from "rxjs/fetch";
 import { $ } from "react-rxjs-elements";
 import "./styles.css";
 import loader from "./assets/loader.gif";
+import refresh from "./assets/refresh.png";
 import generic_flag from "./assets/flag_generic.png";
 
 /*  TODOS: 
@@ -22,83 +23,104 @@ import generic_flag from "./assets/flag_generic.png";
   [9] use Unit tests
 */
 
-const api = `https://restcountries.com/v3.1/all`;
-
-let searchSubject = new BehaviorSubject("");
-let searchResultsObservable = searchSubject.pipe(
-  // console.log("searchSubject :>> ", searchSubject),
-  filter(value => value.length > 1),
-  debounceTime(500),
-  distinctUntilChanged(),
-  mergeMap(value => from(getCountry(value))),
-  tap(ev => console.log("value" + JSON.stringify(ev)))
-);
-
-const getCountry = async (term) => {
-  const allNations = await fetch(api).then((res) => res.json());
-  console.log("allNations : ", allNations);
-  const results = allNations.filter((nation) =>
-    nation.name.common.toLowerCase().includes(term.toLowerCase())
-  );
-  console.log("results>>> : ", results);
-
-  return results;
-};
-
-const useObservable = (observable, setter) => {
-  useEffect(() => {
-    let subscription = observable.subscribe(result => {
-      setter({ result });
-    });
-
-    return () => subscription.unsubscribe();
-  }, [observable, setter]);
-};
+const API = `https://restcountries.com/`;
+const VERSION = 'v3.1/';
+// https://restcountries.com/v2/name/
 
 const App = () => {
   const [search, setSearch] = useState("");
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState([]);
+  const [nations, setNations] = useState([]);
+  const { Map } = require('immutable');
 
-  const customFetch = (url) => {
-    // wait for both fetch and a 500ms timer to finish
-    return zip(
-      fromFetch(url).pipe(switchMap(r => r.json())),
-      timer(500) // set a timer for 500ms
-    ).pipe(
-      // then take only the first value (fetch result)
-      map(([data]) => data),
-      tap(ev => console.log("value>>>>>" + JSON.stringify(ev)))
-    )
+  const init = async (baseURL) => {
+    try {
+      await fetch(baseURL + VERSION + `all`).then((res) => res.json())
+        .then(res => filterData(res));
+    } catch (err) {
+      console.log(err);
+    };
   }
 
-  const stream$ = useMemo(() => // TODO: Fix this
-    customFetch(api).pipe(
-      map(data =>
-        <div>{data.message}</div>,
-        map(data => <div>{data}</div>),
-        catchError(() => of(<div className="err">ERROR</div>)),
-        startWith(<div className="loading">loading...</div>),
-        tap(ev => console.log("data>>" + JSON.stringify(ev)))
-      ))
-    , []);
+  const filterData = async (res) => {
+    try {
+      !isEmpty(res) &&
+        await res.map(nation =>
+          nations.push({
+            id: Math.random().toString(36).slice(-5),
+            name: nation.name.common,
+            flag: !isEmpty(nation.flags) && !isEmpty(nation.flags.svg) //&& isValidURL(nation.flags.svg)
+              ?
+              nation.flags.svg
+              :
+              !isEmpty(nation.flag) && isValidURL(nation.flag)
+                ?
+                nation.flag
+                :
+                generic_flag
+          })
+        );
+      setLoading(true)
+    } catch (err) {
+      console.log(`err`, err)
+      return false;
+    }
+  };
 
-  // useMemo for fetching data on mount
-  useMemo(() => {
-    fetch(api)
-      .then(response => response.json())
-      .then(data => setData(data));
-    setLoading(true);
-  }, []);
+  let fetchSubject = new BehaviorSubject([]);
+  let fetchResultsObservable = fetchSubject.pipe(
+    filter(url => !isEmpty(url)),
+    mergeMap(url => from(init(url))),
+    catchError(() => of(<div className="err">ERROR</div>)),
+  );
 
+  let searchSubject = new BehaviorSubject("");
+  let searchResultsObservable = searchSubject.pipe(
+    filter(url => !isEmpty(url)),
+    debounceTime(500),
+    distinctUntilChanged(),
+    mergeMap(value => from(getCountry(value)))
+  );
+
+  const getCountry = async (name) => {
+    const allNations = await fetch(API + VERSION + `name/${name}`).then((res) => res.json());
+    console.log("allNations : ", allNations);
+    const results = allNations.filter((nation) =>
+      nation.name.common.toLowerCase().includes(name.toLowerCase())
+    );
+    console.log("results>>> : ", results);
+    return results;
+  };
+
+  const useObservable = (observable, setter) => {
+    useEffect(() => {
+      let subscription = observable.subscribe(result => {
+        setter({ result });
+      });
+
+      return () => subscription.unsubscribe();
+    }, [observable, setter]);
+  };
   useObservable(searchResultsObservable, setResults);
+
+  useObservable(fetchResultsObservable, setData);
 
   const handleSearchChange = (e) => {
     const newValue = e.target.value;
     setSearch(newValue);
-    searchSubject.next(newValue);
   };
+
+  // useMemo for fetching data on mount
+  useMemo(() => {
+    fetchSubject.next(API);
+  }, []);
+
+  // useMemo for search results
+  useMemo(() => {
+    searchSubject.next(search);
+  }, [search]);
 
   return (
     <div className="App">
@@ -111,21 +133,22 @@ const App = () => {
       />
       <div className="container">
         <div className="list-item">
-          <$>{stream$}</$>
           <>
             {search.length <= 1 && loading ?
-              data.map(nation =>
-                <div key={nation.name.common}>
-                  <img src={nation.flags.svg ? nation.flags.svg : generic_flag} style={{ width: 26 }} alt="flag" />
-                  {`  ${nation.name.common}`}
+              nations.map(nation =>
+                <div key={nation.id}>
+                  <img key={nation.id} src={nation.flag} style={{ width: 26 }} alt="flag" />
+                  {`    ${nation.name}    `}
+                  <img src={refresh} style={{ width: 14 }} alt="Refresh image" />
                 </div>
               )
               :
               search.length > 1 && results.length > 0 ?
                 results.map(nation =>
-                  <div key={nation.name.common}>
-                    <img src={nation.flags.svg ? nation.flags.svg : generic_flag} style={{ width: 26 }} alt="flag" />
-                    {`  ${nation.name.common}`}
+                  <div key={nation.id}>
+                    <img key={nation.id} src={nation.flag} style={{ width: 26 }} alt="flag" />
+                    {`    ${nation.name}    `}
+                    <img src={refresh} style={{ width: 14 }} alt="Refresh image" />
                   </div>
                 )
                 :
@@ -140,5 +163,22 @@ const App = () => {
     </div >
   );
 };
+
+
+const isValidURL = async (url) => {
+  try {
+    const result = await fetch(url, { method: 'HEAD' });
+    if (result.ok == true)
+      return true;
+    else return false;
+  } catch (err) {
+    console.log(`url not valid: ${url}`)
+    return false;
+  }
+};
+
+const isEmpty = (val) => {
+  return (val == undefined || val == null || val.length < 1) ? true : false;
+}
 
 export default App;
